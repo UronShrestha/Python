8d92c39d284152c85c3d6be44af23b036d39f529
UronShrestha <yuron.stha57@gmail.com>
2026-02-16 17:34:53 +0545

    counting in reverse using for loop

--- forLoops/forLoops.py
+++ forLoops/forLoops.py
@@ -1,5 +1,12 @@
 # for loops = execute a block of code a fixed number of times. 
 #             you can iterate over a range, string, sequence, etc.
 
+#e.g 1: count 1 to 10
+print("Counting forward from 1 to 10:")
 for x in range(1,11):
+    print(x)
+print("")
+# e.g 2: count 10 to 1 in reverse
+print("Counting reverse from 1 to 10:")
+for x in reversed(range(1, 11)):
     print(x)
