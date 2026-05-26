8078228d1d7a93afc90c05a3ad1ee1c8574d2870
UronShrestha <yuron.stha57@gmail.com>
2026-05-26 13:57:25 +0545

    changes in oneSimplePasswordChecker

--- whileLoop/Mini Projects Using Loops/oneSimplePasswordChecker.py
+++ whileLoop/Mini Projects Using Loops/oneSimplePasswordChecker.py
@@ -1,17 +1,18 @@
 correct_password = "Password"
 
-# while True:
-#     pwd = input("Enter the password : ")
-#     if pwd == correct_password:
-#         print("Welcome!")
-#         break
-#     else:
-#         print("Wrong Password!") 
+while True:
+    pwd = input("Enter the password : ")
+    if pwd == correct_password:
+        print("Access Granted!")
+        break
+    else:
+        print("Wrong Password!") 
 
-pwd = input("\nEnter the password : ")
+# correct_password = input("Create new password : ")
+# pwd = input("\nEnter the password : ")
 
-while pwd != correct_password:
-    print("\nPassword Incorrect!")
-    pwd = input("Enter the password again : ")
+# while pwd != correct_password:
+#     print("\nPassword Incorrect!")
+#     pwd = input("Enter the password again : ")
 
-print("Access Granted!")
+# print("Access Granted!")
