dc62991c2feee24e9cba85b3a1a7580ae041d30a
UronShrestha <yuron.stha57@gmail.com>
2026-05-30 15:02:17 +0545

    Enter the number of rows and columns pattern

---
+++ loops/nestedLoops/example2Symbol.py
@@ -0,0 +1,12 @@
+rows = int(input("Enter the number of rows : "))
+columns = int(input("Enter the number of columns : "))
+symbol = input("Input the desired symbol : ")
+
+for row in range(rows):
+    for column in range(columns):
+        print(symbol, end="")
+    print()
+
+     
+     
+
